1ecb044d96d09402ee3fd7ef6f3a577b74d00f9d
Gregor von Laszewski <laszewski@gmail.com>
2015-08-12 12:27:37 -0400

    simplify vagrant file

--- slurm/post.sh
+++ slurm/post.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-
-
 rm -f ~/.ssh/known_hosts
 
 export NOSTRICT="-o StrictHostKeyChecking=no"
@@ -20,11 +18,9 @@ master)
     $SCP test.sh $MASTER
     $SSH $MASTER 'sudo /usr/sbin/create-munge-key -f'
     $SSH $MASTER 'sudo cat /etc/munge/munge.key' > munge.key
-    $SCP munge.service $MASTER:/home/vagrant/
-    $SSH $MASTER 'sudo cp ~/shared/munge.service /etc/systemd/system/munge.service'
     $SSH $MASTER sudo systemctl enable --system --full munge
-    # $SSH $MASTER sudo systemctl daemon-reload
     $SSH $MASTER sudo systemctl start munge
+    $SSH $MASTER sudo slurmctld -D &
     ;;
 
 worker)
@@ -32,21 +28,9 @@ worker)
     $SCP test.sh $WORKER
     $SCP munge.key $WORKER:/home/vagrant/
     $SSH $WORKER 'sudo cp ~/munge.key /etc/munge; sudo chown munge /etc/munge/munge.key'
-    $SCP munge.service $WORKER:/home/vagrant/
-    $SSH $WORKER 'sudo cp ~/shared/munge.service /etc/systemd/system/munge.service'
     $SSH $WORKER sudo systemctl enable --system --full munge
-    # $SSH $WORKER sudo systemctl daemon-reload
     $SSH $WORKER sudo systemctl start munge
-    ;;
-
-sworker)
-    set -x
     $SSH $WORKER sudo slurmd &
-    ;;
-
-smaster)
-    set -x
-    $SSH $MASTER sudo slurmctld -D &
     ;;
 
 status)
